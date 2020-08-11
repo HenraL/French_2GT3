4081d36bc328854b88f66b6d8ff8f0a672432642
HenraL <37299738+HenraL@users.noreply.github.com>
2020-08-11 10:14:10 +0200

    uploaded the files

---
+++ files/2GT3/010 TPE Français/Henry.bat
@@ -0,0 +1,28 @@
+color 0A
+echo off
+cls
+cd "C:\Users\ELEVE\Documents\Documents\Matières\Français\2GT3\010 TPE Français\Henry.bat"
+REM pause
+echo Les mythes (comme celui de Prométhée, celui d’Ulysse ou d’Hercule) sont une manière d’interroger sans prétendre savoir ce que l’homme ne sait pas. C’est une humble façon de chercher des explications. Depuis que les hommes réfléchissent aux grands mystères de leurs conditions Ils utilisent les mythes. Et pour les expliquer, nous allons étudier les mythes antiques, suivit par les modernes et leurs importance dans les sociétés. Puis nous analyserons leurs différents impacts et chercherons à les expliquer.
+echo .
+echo .
+echo   1. Les caractéristiques des mythes modernes
+echo  Depuis le début de la modernité, les penseurs occidentaux découvrent trouvent chez tous les peuples des récits jugés importants, récits fondateurs ou exemplaires, qui sont préservés à travers les générations, qui se démarquent par leur élaboration ou par leur mise en scène rituelle, qui sont composés de situations, êtres, événements en dehors des règles réelles et de l’expérience quotidienne de la société en question. De plus, avec l'apparition d'Internet et des réseaux sociaux, les mythes modernes sont mondiaux et plus seulement régionaux. Par exemple Le mythe du rêve américains par exemple, est parvenu jusqu'en France et à fait parler de lui partout dans le monde. Contrairement à ce que l'on pourrait croire, le monde moderne a produit autant de mythes que l'Antiquité. En voici quelques-uns :
+echo Le monstre créé par le docteur Frankenstein, rappelle une vieille légende juive, celle du Golem : un vieux kabbaliste avait créé, avec de la terre et des formules magiques un être. Il avait gravé sur son front le mot « Emeth » (connaissance) ; quand la créature devint meurtrière, il effaça la première lettre, formant le mot « meth » (mort). Il ne resta qu'un tas de terre.
+echo  Don Quichotte est le héros d'un livre de Miguel Cervantes. Pauvre fou qui se prend pour un chevalier et part sur les routes pour sauver le monde, il est aussi le symbole de la puissance du rêve et de la force du désir. Son écuyer Sancho Panza (panse) représente les appétits matériels.
+echo  Hamlet (shakespeare) est le type de l'homme irrésolu, dont l'attitude incompréhensible sème le trouble, la mort et la folie autour de lui.
+echo  Don Juan est l'archétype du séducteur. Mais, plus qu'amoureux des femmes, il les séduit pour défier, à travers ses innombrables conquêtes, l'ordre du monde et celui des dieux. Il sera entraîné en enfer par le fantôme d'un père qu'il a tué en duel, mais Don Juan meurt sans s'être jamais repenti.
+echo .
+echo .
+echo  succès mondial.
+echo Elvis Presley et Bob Marley font eux aussi toujours partis de ceux que l'on n'oubliera jamais, ceux qui ont laissé leurs traces dans notre vie quotidienne, et dans l'univers culturel de notre société. Les mythes modernes apparaissent sous toutes les formes, y compris dans la bande dessinée et les comics. Ce genre de support popularise souvent des héros, se rapprochant plus des mythes antiques que les mythes que nous avons vue précédemment. Mais la définition d'un héros de mythes moderne change légèrement par rapport à la définition d'un héros de mythes antiques :
+echo  Un héros est un personnage qui se distingue par ses actes, son courage et l'abnégation dont il fait preuve lorsqu'il les réalise. Il peut être réel ou fictif. Quand les faits qui leurs sont attribués sont exemplaires, ils entrent dans l'Histoire, ils sont l'objet de légende et certain vont jusqu'à leur vouer un culte.
+echo  Dans le cas des Bandes dessinées, les héros sont des êtres de fiction qui se distinguent par leurs pouvoirs hors du commun. Ils ont des capacités qui dépassent l'entendement, ce sont des super humains si bien qu'on les désigne comme des super-héros.
+echo  Ils nous apparaissent comme des justiciers.
+echo   Les héros de mythes antiques ne possédait pas de pouvoirs, ce sont leur dieux qui les aidait ou leurs en conférait temporairement.
+echo .
+echo .
+echo  Les héros de mythes antiques avaient tous vécu pour les personnes vivants à leur époques, et l'existence des dieux ou la véracité des récits ne faisait aucun doute pour eux, ce qui rends l'impact des mythes antiques très importants. Pendant l'Antiquité, culture et religion était très liée mais aujourd'hui, seul les mythes culturels sont présents. On pourrait donc penser que les mythes modernes ont moins d'influence qu'en on eu les mythes antiques, mais les mythes modernes sont aussi très présents à l'aide de personnages ressemblant au héros de mythes antiques.
+echo .
+echo  Les super-héros des comics, des bande-dessinée ou des dessin-animée ressemblent fortement au héros de mythes antique tels que Hercule, Thésée ou encore Achille. Ces héros ont des pouvoirs, et sont chargée d'accomplir une quête, à l'instar des héros de mythes antiques, mais en excluant le coté religieux des pouvoirs conférés par des dieux.
+REM pause
